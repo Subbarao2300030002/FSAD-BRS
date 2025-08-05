f39b3622f9d6860f8809145420361bc894c09b34
aaki bhawanisubbaro <2300030002@kluniversity.in>
2025-08-05 16:11:48 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>GITHUB PAGES</h1> {/* Updated heading */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
